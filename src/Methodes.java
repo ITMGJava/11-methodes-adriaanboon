c15568aa58bf345abde6b48622c5d782d5837722
adriaanboon <71772080+adriaanboon@users.noreply.github.com>
2020-10-21 15:22:43 +0200

    Opdracht Methodes eerste deel.

--- src/Methodes.java
+++ src/Methodes.java
@@ -1,2 +1,39 @@
+import java.util.Scanner;
+
 public class Methodes {
+
+    public static void main(String[] args) {
+        helloWorld();
+        helloUser();
+        helloIngevoerdenaam();
+        berekenEindbedrag(1,1);
+    }
+
+    static void helloWorld() {
+        System.out.println("Hello world");
+    }
+    static void helloUser() {
+        System.out.println("Hello, User");
+    }
+    static void helloIngevoerdenaam() {
+        System.out.println("Wat is je naam?");
+        var keyboard = new Scanner(System.in);
+        String invoerNaam = keyboard.nextLine();
+        System.out.println("Hello "+invoerNaam);
+    }
+    static float berekenEindbedrag(float beginBedrag, float maandRente) {
+        double samengestelderenteNaTienJaar;
+        double totaalBedragIncRente;
+        System.out.println("Voer een bedrag in:");
+        var bedrag = new Scanner(System.in);
+        double invoerBedrag = bedrag.nextFloat();
+        System.out.println("Voer een maandrente in:");
+        var rente = new Scanner(System.in);
+        float invoerRente = rente.nextFloat();
+        double percentageRente = invoerRente/100+1;
+        samengestelderenteNaTienJaar = Math.pow(10, percentageRente);
+        totaalBedragIncRente = samengestelderenteNaTienJaar * invoerBedrag;
+        System.out.println(totaalBedragIncRente);
+        return 1;
+    }
 }
